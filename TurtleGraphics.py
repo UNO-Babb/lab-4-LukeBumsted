d6a813bfbd1229f68b88e252914c20e27fdacfcc
LukeBumsted <Labumsted@gmail.com>
2024-09-18 11:34:11 -0500

    Update TurtleGraphics.py

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -11,13 +11,54 @@ def drawSquare(myTurtle, size):
         myTurtle.forward(size)
         myTurtle.right(90)
 
-
-def main():
+def drawPolygon(myTurtle, sides):
+    for i in range(sides):
+        myTurtle.forward(100)
+        myTurtle.right(360/sides)
+        
+def fillCorner(myTurtle, corner):
+    drawSquare(myTurtle, 100)
+    
+    if corner == 2:
+        myTurtle.forward(50)
+    elif corner == 3:
+        myTurtle.right(90)
+        myTurtle.forward(50)
+        myTurtle.left(90)
+    elif corner == 4:
+        myTurtle.right(90)
+        myTurtle.forward(100)
+        myTurtle.left(90)
+        myTurtle.forward(50)
+        myTurtle.left(90)
+        
+    myTurtle.begin_fill()
+    drawSquare(myTurtle, 50)
+    myTurtle.end_fill()
+    
+def squaresInSquares(myTurtle, num):
+    size = 10
+    for i in range(num):
+        drawSquare(myTurtle,size)
+        size = size + 20
+        myTurtle.right(180)
+        myTurtle.up()
+        myTurtle.forward(10)
+        myTurtle.right(90)
+        myTurtle.forward(10)
+        myTurtle.right(90)
+        myTurtle.down()
+    
+def main(): 
     myTurtle = turtle.Turtle()
+    
+    squaresInSquares(myTurtle, 10)
+
+    
     # drawPolygon(myTurtle, 5) #draws a pentagon
     # drawPolygon(myTurtle, 8) #draws an octogon
 
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
+    #fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
     # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
 
     # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
